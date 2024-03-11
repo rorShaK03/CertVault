e12a503b0999f3c8e0fd118e53b21756deb079ee
lyricsred <106448247+lyricsred@users.noreply.github.com>
2024-03-11 11:06:42 +0300

    Add files via upload
    Simple authorization

--- src/main/java/ru/hse/CertVault.java
+++ src/main/java/ru/hse/CertVault.java
@@ -2,6 +2,7 @@ package ru.hse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class CertVault {
     public static void main(String[] args) {

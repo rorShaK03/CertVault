4ef41800b1076c9a4879d54b59278859824aeffb
Shabad Kirill <shabad-kirill@yandex-team.ru>
2024-04-12 04:47:25 +0300

    Some commit

--- src/main/java/ru/hse/CertVault.java
+++ src/main/java/ru/hse/CertVault.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CertVault {
-    // Some code
+    // Some comment
     public static void main(String[] args) {
         SpringApplication.run(CertVault.class, args);
     }
